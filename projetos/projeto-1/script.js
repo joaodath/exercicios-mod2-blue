d9e1ef9ded7b2e678337b4b3afd6bbbd3a905d48
João Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-07-18 11:28:33 -0300

    minor changes

--- projetos/projeto-1/script.js
+++ projetos/projeto-1/script.js
@@ -1,51 +1,73 @@
+// Gets the current doctor image, mood text, button and icons displayed on screen
 const imagemDoutor = document.getElementById('img-card');
-const humorDoutor = document.getElementById('img-humor');
-const botaoHumorDoutor = document.getElementById('btn-humor');
+const moodDoutor = document.getElementById('img-mood');
+const botaomoodDoutor = document.getElementById('btn-mood');
+const icon = document.getElementById('icon');
+const shortcuticon = document.getElementById('shortcut-icon');
 
-botaoHumorDoutor.onclick = function () {
-    if (botaoHumorDoutor.value === 'happy1') {
-        botaoHumorDoutor.value = 'curious';
+// Changes the current doctor image, mood text, button and icons displayed on screen
+botaomoodDoutor.onclick = function () {
+    if (botaomoodDoutor.value === 'happy1') {
+        botaomoodDoutor.value = 'curious';
         imagemDoutor.src = 'img/doctor-curious.jpg';
-        humorDoutor.textContent = 'The Doctor is curious!';
+        moodDoutor.textContent = 'The Doctor is curious!';
+        icon.href = 'img/doctor-curious.jpg';
+        shortcuticon.href = 'img/doctor-curious.jpg';
     }
-    else if (botaoHumorDoutor.value === 'curious') {
-        botaoHumorDoutor.value = 'sad';
+    else if (botaomoodDoutor.value === 'curious') {
+        botaomoodDoutor.value = 'sad';
         imagemDoutor.src = 'img/doctor-sad.jpg';
-        humorDoutor.textContent = 'The Doctor is sad!';
+        moodDoutor.textContent = 'The Doctor is sad!';
+        icon.href = 'img/doctor-sad.jpg';
+        shortcuticon.href = 'img/doctor-sad.jpg';
     }
-    else if (botaoHumorDoutor.value === 'sad') {
-        botaoHumorDoutor.value = 'happy2';
+    else if (botaomoodDoutor.value === 'sad') {
+        botaomoodDoutor.value = 'happy2';
         imagemDoutor.src = 'img/doctor-happy2.jpg';
-        humorDoutor.textContent = 'The Doctor is happy!';
+        moodDoutor.textContent = 'The Doctor is happy!';
+        icon.href = 'img/doctor-happy2.jpg';
+        shortcuticon.href = 'img/doctor-happy2.jpg';
     }
-    else if (botaoHumorDoutor.value === 'happy2') {
-        botaoHumorDoutor.value = 'naughty';
+    else if (botaomoodDoutor.value === 'happy2') {
+        botaomoodDoutor.value = 'naughty';
         imagemDoutor.src = 'img/doctor-naughty.jpg';
-        humorDoutor.textContent = 'The Doctor is naughty!';
+        moodDoutor.textContent = 'The Doctor is naughty!';
+        icon.href = 'img/doctor-naughty.jpg';
+        shortcuticon.href = 'img/doctor-naughty.jpg';
     }
-    else if (botaoHumorDoutor.value === 'naughty') {
-        botaoHumorDoutor.value = 'happy3';
+    else if (botaomoodDoutor.value === 'naughty') {
+        botaomoodDoutor.value = 'happy3';
         imagemDoutor.src = 'img/doctor-happy3.jpg';
-        humorDoutor.textContent = 'The Doctor is happy!';
+        moodDoutor.textContent = 'The Doctor is happy!';
+        icon.href = 'img/doctor-happy3.jpg';
+        shortcuticon.href = 'img/doctor-happy3.jpg';
     }
-    else if (botaoHumorDoutor.value === 'happy3') {
-        botaoHumorDoutor.value = 'angry';
+    else if (botaomoodDoutor.value === 'happy3') {
+        botaomoodDoutor.value = 'angry';
         imagemDoutor.src = 'img/doctor-angry.jpg';
-        humorDoutor.textContent = 'The Doctor is angry!';
+        moodDoutor.textContent = 'The Doctor is angry!';
+        icon.href = 'img/doctor-angry.jpg';
+        shortcuticon.href = 'img/doctor-angry.jpg';
     }
-    else if (botaoHumorDoutor.value === 'angry') {
-        botaoHumorDoutor.value = 'happy4';
+    else if (botaomoodDoutor.value === 'angry') {
+        botaomoodDoutor.value = 'happy4';
         imagemDoutor.src = 'img/doctor-happy4.jpg';
-        humorDoutor.textContent = 'The Doctor is happy!';
+        moodDoutor.textContent = 'The Doctor is happy!';
+        icon.href = 'img/doctor-happy4.jpg';
+        shortcuticon.href = 'img/doctor-happy4.jpg';
     }
-    else if (botaoHumorDoutor.value === 'happy4') {
-        botaoHumorDoutor.value = 'sad2';
+    else if (botaomoodDoutor.value === 'happy4') {
+        botaomoodDoutor.value = 'sad2';
         imagemDoutor.src = 'img/doctor-sad2.jpg';
-        humorDoutor.textContent = 'The Doctor is sad!';
+        moodDoutor.textContent = 'The Doctor is sad!';
+        icon.href = 'img/doctor-sad2.jpg';
+        shortcuticon.href = 'img/doctor-sad2.jpg';
     }
-    else if (botaoHumorDoutor.value === 'sad2') {
-        botaoHumorDoutor.value = 'happy1';
+    else if (botaomoodDoutor.value === 'sad2') {
+        botaomoodDoutor.value = 'happy1';
         imagemDoutor.src = 'img/doctor-happy.jpg';
-        humorDoutor.textContent = 'The Doctor is happy!';
+        moodDoutor.textContent = 'The Doctor is happy!';
+        icon.href = 'img/doctor-happy.jpg';
+        shortcuticon.href = 'img/doctor-happy.jpg';
     }
 }
