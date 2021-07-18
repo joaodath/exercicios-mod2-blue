279d831e4330e247551bc930a32aa7490cc9736a
João Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-07-18 16:13:43 -0300

    added language options

--- projetos/projeto-1/script-br.js
+++ projetos/projeto-1/script-br.js
@@ -5,68 +5,68 @@ const btnMoodDoctor = document.getElementById('btn-mood');
 const icon = document.getElementById('icon');
 const shortcuticon = document.getElementById('shortcut-icon');
 
-// Changes the current doctor image, mood text, button and icons displayed on screen
+// Changes the current doctor image, mood text, button and icons displayed on screen based on language
 btnMoodDoctor.onclick = function () {
     if (btnMoodDoctor.value === 'happy1') {
         btnMoodDoctor.value = 'curious';
         imgDoctor.src = 'img/doctor-curious.jpg';
-        moodDoctor.textContent = 'The Doctor is curious!';
+        moodDoctor.textContent = 'A Doutora está curiosa!';
         icon.href = 'img/doctor-curious.jpg';
         shortcuticon.href = 'img/doctor-curious.jpg';
     }
     else if (btnMoodDoctor.value === 'curious') {
         btnMoodDoctor.value = 'sad';
         imgDoctor.src = 'img/doctor-sad.jpg';
-        moodDoctor.textContent = 'The Doctor is sad!';
+        moodDoctor.textContent = 'O Doutor está triste!';
         icon.href = 'img/doctor-sad.jpg';
         shortcuticon.href = 'img/doctor-sad.jpg';
     }
     else if (btnMoodDoctor.value === 'sad') {
         btnMoodDoctor.value = 'happy2';
         imgDoctor.src = 'img/doctor-happy2.jpg';
-        moodDoctor.textContent = 'The Doctor is happy!';
+        moodDoctor.textContent = 'O Doutor está feliz!';
         icon.href = 'img/doctor-happy2.jpg';
         shortcuticon.href = 'img/doctor-happy2.jpg';
     }
     else if (btnMoodDoctor.value === 'happy2') {
         btnMoodDoctor.value = 'naughty';
         imgDoctor.src = 'img/doctor-naughty.jpg';
-        moodDoctor.textContent = 'The Doctor is naughty!';
+        moodDoctor.textContent = 'O Doutor está safadinho!';
         icon.href = 'img/doctor-naughty.jpg';
         shortcuticon.href = 'img/doctor-naughty.jpg';
     }
     else if (btnMoodDoctor.value === 'naughty') {
         btnMoodDoctor.value = 'happy3';
         imgDoctor.src = 'img/doctor-happy3.jpg';
-        moodDoctor.textContent = 'The Doctor is happy!';
+        moodDoctor.textContent = 'O Doutor está feliz!';
         icon.href = 'img/doctor-happy3.jpg';
         shortcuticon.href = 'img/doctor-happy3.jpg';
     }
     else if (btnMoodDoctor.value === 'happy3') {
         btnMoodDoctor.value = 'angry';
         imgDoctor.src = 'img/doctor-angry.jpg';
-        moodDoctor.textContent = 'The Doctor is angry!';
+        moodDoctor.textContent = 'O Doutor está com raiva';
         icon.href = 'img/doctor-angry.jpg';
         shortcuticon.href = 'img/doctor-angry.jpg';
     }
     else if (btnMoodDoctor.value === 'angry') {
         btnMoodDoctor.value = 'happy4';
         imgDoctor.src = 'img/doctor-happy4.jpg';
-        moodDoctor.textContent = 'The Doctor is happy!';
+        moodDoctor.textContent = 'O Doutor está feliz!';
         icon.href = 'img/doctor-happy4.jpg';
         shortcuticon.href = 'img/doctor-happy4.jpg';
     }
     else if (btnMoodDoctor.value === 'happy4') {
         btnMoodDoctor.value = 'sad2';
         imgDoctor.src = 'img/doctor-sad2.jpg';
-        moodDoctor.textContent = 'The Doctor is sad!';
+        moodDoctor.textContent = 'O Doutor está feliz!';
         icon.href = 'img/doctor-sad2.jpg';
         shortcuticon.href = 'img/doctor-sad2.jpg';
     }
     else if (btnMoodDoctor.value === 'sad2') {
         btnMoodDoctor.value = 'happy1';
         imgDoctor.src = 'img/doctor-happy.jpg';
-        moodDoctor.textContent = 'The Doctor is happy!';
+        moodDoctor.textContent = 'O Doutor está feliz!';
         icon.href = 'img/doctor-happy.jpg';
         shortcuticon.href = 'img/doctor-happy.jpg';
     }
